1f0a5e4400e4a4e1ed7021cd696491060aea7a63
LeoLeerila <leo.leerila@metropolia.fi>
2026-04-24 09:28:59 +0300

    add games

---
+++ casino/src/main/java/game/Billiards.java
@@ -0,0 +1,7 @@
+package game;
+
+public class Billiards extends GameAbstract {
+    public Billiards(int maxPlayers, int minBet, double cashOutMult, double winChance, int gameTime){
+        super(maxPlayers, minBet, cashOutMult, winChance, gameTime);
+    }
+}
---
+++ casino/src/main/java/game/Slotmachine.java
@@ -0,0 +1,7 @@
+package game;
+
+public class Slotmachine extends GameAbstract {
+    public Slotmachine(int maxPlayers, int minBet, double cashOutMult, double winChance, int gameTime){
+        super(maxPlayers, minBet, cashOutMult, winChance, gameTime);
+    }
+}
